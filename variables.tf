40ceb47db82fafb8707abbbe8eea42179f31db7f
tf-vishal <vvishalsharma0609@gmail.com>
2025-08-06 09:51:19 +0530

    variables

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "ec2_instance_type" {
+    default = "t2-micro"
+    type = string
+}
+
+variable "ec2_root_storage_size" {
+    default = 10
+    type = number
+}
+
+variable "ec2_ami_id" {
+    default = "ami-0f918f7e67a3323f0"
+    type = string
+}
