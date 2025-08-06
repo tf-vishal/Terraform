9f693af33db63272303a912e95a7a84fde359ab5
tf-vishal <vvishalsharma0609@gmail.com>
2025-08-06 18:04:52 +0530

    removed the s3 and dynomo state feature

--- terraform.tf
+++ terraform.tf
@@ -5,9 +5,4 @@ terraform {
       version = "6.7.0"
     }
   }
-#   bucket = "remote-bucket-backend-12312323132132"
-#    key = "terraform.tfstate"
-#    region = "ap-south-1"
-#    dynamodb_table = "remote-state-table"
-#  }
 }
