3cd822909b5b1dbedd867d6bd686e3e33ae918b7
tf-vishal <vvishalsharma0609@gmail.com>
2025-08-06 17:45:14 +0530

    state locking on bucket s3 with dynomo db

--- terraform.tf
+++ terraform.tf
@@ -5,4 +5,10 @@ terraform {
       version = "6.7.0"
     }
   }
+  backend "s3" {
+    bucket = "remote-bucket-backend-12312323132132"
+    key = "terraform.tfstate"
+    region = "ap-south-1"
+    dynamodb_table = "remote-state-table"
+  }
 }
