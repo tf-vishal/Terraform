4828a5923f6a15c20d721024c695c19eeb4762f4
tf-vishal <vvishalsharma0609@gmail.com>
2025-08-06 11:15:38 +0530

    dynamic-blocks/meta-arguments

--- ec2.tf
+++ ec2.tf
@@ -42,6 +42,7 @@ resource "aws_security_group" "my_sg"{
 #Ec2 Instance
 
 resource "aws_instance" "my_instance"{
+    count = 1 # creates number of resources of thre respective block, here it is instance
     key_name = aws_key_pair.my_key.key_name
     security_groups = [aws_security_group.my_sg.name]
     instance_type = var.ec2_instance_type
--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "ec2_public_ip" {
-    value = aws_instance.my_instance.public_ip
-}
+    value = aws_instance.my_instance[*].public_ip # [*] is used if instance are more than 1
+} #we will use aws_instance.my_instance.public_ip for single instance
 
 output "ec2_public_dns" {
-    value = aws_instance.my_instance.public_dns
+    value = aws_instance.my_instance[*].public_dns
 }
 
 output "ec2_private_ip" {
-    value = aws_instance.my_instance.private_ip
+    value = aws_instance.my_instance[*].private_ip
 }
